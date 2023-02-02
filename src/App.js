503429f958f25eef70e346fa4b243b83d3b933fb
김선인 <kimsunin@gimseon-in-ui-MacBookAir.local>
2023-02-02 17:58:44 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          선인ㅎㅇ <code>src/App.js</code> and save to reload.
+          선인ㅎㅇ 어쩔<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
