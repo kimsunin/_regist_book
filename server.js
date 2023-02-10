dca436e3e336b951d4b7f295a5b655ea124a4d56
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-02-10 17:29:20 +0900

    react에서 post로 username,password받기

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ import cors from "cors";
 import bodyParser from "body-parser"; //post로 받은 데이터의 body부분을 읽기위해서 안쓰면 제대로 못읽는다
 const app = express();
 
+const user = [
+  {
+    username: "changmin",
+    password: "1234",
+  },
+  {
+    username: "sunin",
+    password: "1234",
+  },
+];
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -11,17 +22,17 @@ app.use(cors());
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello express" });
-});
-app.post("/", (req, res) => {
-  const name = req.body;
-  console.log("받아졌음");
-  console.log(req.body);
-});
-
-app.get("/", (req, res) => {
+const postLogin = (req, res) => {
+  const postUsername = req.body.username;
+  const postPassword = req.body.password;
+  console.log(postUsername, postPassword, "받아졌음");
+};
+const getLogin = (req, res) => {
   res.render("a.html");
-});
+};
+app.route("/").get(getLogin).post(postLogin);
+// app.post("/", postLogin);
+
+// app.get("/", getLogin);
 
 app.listen(4000, () => console.log("✅server is listening!"));
